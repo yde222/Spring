0e884bea7a7bac00c5df0e446698ed84d78eebcf
yde222 <yudaeun222@gmail.com>
2025-06-07 05:17:11 +0900

    롬복 적용

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,26 +6,20 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+
 @Component
+@RequiredArgsConstructor
 @Primary
 public class OrderServiceImpl implements OrderService{
 
     private  final MemberRepository memberRepository;
     private  final DiscountPolicy discountPolicy;
-
-
-    //    @Autowired
-//    생성자- 불변,필수 의존관계
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
