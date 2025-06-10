8fd8daf32d41d4d5be0b43ac216f5ccc41df3ab9
jieun <yudaeun222@gmail.com>
2025-06-10 22:37:08 +0900

    회원 등록

--- jpashop/src/main/java/jpabook/jpashop/HelloController.java
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
