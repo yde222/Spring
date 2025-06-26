10d136b550b519a4d0b80f60cb4b1cca1fc4a50c
jieun <yudaeun222@gmail.com>
2025-06-26 22:00:15 +0900

    수정

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,11 +13,20 @@ public class JpaMain {
         tx.begin();
         //code
         try {
+//             update 쿼리가 조회되지 않을 경우 삽입
             Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            member.setId(11);
+            member.setName("jieun");
 
+            //등록
             em.persist(member);
+          
+            member.setName("지은");
+
+
+            Member findMember = em.find(Member.class,1L);
+
+            findMember.setName("HelloJPA");
 
             tx.commit();
         } catch (Exception e){
