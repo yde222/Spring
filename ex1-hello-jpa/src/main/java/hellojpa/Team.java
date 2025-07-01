215de628d898e53245f5adc05fd993bf54d33774
jieun <yudaeun222@gmail.com>
2025-07-01 14:10:23 +0900

    다대일 양방향

--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -13,6 +13,9 @@ public class Team {
     private Long id;
     private String name;
 
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
     public Long getId() {
         return id;
     }
