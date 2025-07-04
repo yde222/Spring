a464ba61050279f7529929b4020498ec10a6eeb1
jieun <yudaeun222@gmail.com>
2025-07-04 14:30:33 +0900

    @Inheritance(strategy = InheritanceType.SINGLE_TABLE)

--- ex1-hello-jpa/src/main/java/hellojpa/Item.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -3,7 +3,7 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="DIS_TYPE")
 public class Item {
 
