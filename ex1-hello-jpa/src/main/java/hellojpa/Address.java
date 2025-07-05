3b44c40a62fa8545e739326b16bf17ae70053db4
jieun <yudaeun222@gmail.com>
2025-07-05 12:46:22 +0900

    값 동일성(==), 동등성equals 비교

--- ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
+
 @Embeddable
 public class Address {
     private String city;
@@ -39,5 +41,17 @@ public class Address {
 
     private void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address address)) return false;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
     }
 }
