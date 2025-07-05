11557c78bfc126cda74594c07f97ec2ca113f18d
jieun <yudaeun222@gmail.com>
2025-07-05 23:03:01 +0900

    left (outer) join

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -29,7 +29,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from  Member m inner join m.team t ";
+            String query = "select m from  Member m left join m.team t ";
 
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
