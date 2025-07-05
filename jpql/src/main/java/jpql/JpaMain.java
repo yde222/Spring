1bd9e356cd0ab7056da685eacce6ec32b4faac8f
jieun <yudaeun222@gmail.com>
2025-07-05 17:54:00 +0900

    TypeQuery,Query

---
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,39 @@
+package jpql;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<Member> query2 = em.createQuery("select m.username,m.age from Member m", Member.class);
+            Query query3 = em.createQuery("select m.username,m.age from Member m", Member.class);
+
+
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+            emf.close();
+    }
+}
---
+++ jpql/src/main/java/jpql/Product.java
@@ -0,0 +1,20 @@
+package jpql;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private int price;
+    private int stockAmount;
+
+
+
+
+}
