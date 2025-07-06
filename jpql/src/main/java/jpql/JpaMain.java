e1b2b0087820badd3d20e0dc6f3a7ddddeb3184a
jieun <yudaeun222@gmail.com>
2025-07-06 20:23:21 +0900

    fetch join

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -17,27 +17,47 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m from Team t join t.members m";
-            List<Collection> result = em.createQuery(query, Collection.class)
+            String query = "select m from Member m join fetch m.team";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
+            
+            for(Member member : result){
+                System.out.println("member = " + member.getUsername() + " , " + member.getTeam().getName());
+                //회원1, 팀A(SQL)
+                //회원2, 팀A(1차캐시)
+                //회원3, 팀B(SQL)
 
-            System.out.println("result = " + result);
+                //회원 100명 -> N + 1 ... 1 + N 이라 봐도 무방
+
+            }
+
+
 
             tx.commit();
         } catch (Exception e){
