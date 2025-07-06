7f45f6a4cf8f8d129f2b33f150b618a9e7ff56c4
jieun <yudaeun222@gmail.com>
2025-07-06 20:49:21 +0900

    컬렉션 페치 조인

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -43,21 +43,16 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m join fetch m.team";
-            List<Member> result = em.createQuery(query, Member.class)
+            String query = "select t from Team t join fetch t.members";
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
             
-            for(Member member : result){
-                System.out.println("member = " + member.getUsername() + " , " + member.getTeam().getName());
-                //회원1, 팀A(SQL)
-                //회원2, 팀A(1차캐시)
-                //회원3, 팀B(SQL)
-
-                //회원 100명 -> N + 1 ... 1 + N 이라 봐도 무방
-
+            for(Team team : result){
+                System.out.println("team = " + team.getName() + "|members = " + team.getMembers().size());
+                for(Member member : team.getMembers()) {
+                    System.out.println(" -> member = " + member);
+                }
             }
-
-
 
             tx.commit();
         } catch (Exception e){
