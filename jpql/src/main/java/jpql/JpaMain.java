b55f1078760ec5a8d5fea0d2453c256ff3c53e00
jieun <yudaeun222@gmail.com>
2025-07-06 19:50:39 +0900

    collection 값 연관 경로

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -3,6 +3,7 @@ package jpql;
 import jakarta.persistence.*;
 
 import javax.lang.model.SourceVersion;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -16,26 +17,27 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            em.persist(team);
 
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-
-            String query = " select m.team FROM Member m" ;
-            List<Team> result = em.createQuery(query, Team.class)
+            String query = "select t.members from Team t";
+            Collection result = em.createQuery(query, Collection.class)
                     .getResultList();
 
-            for(Team s : result){
-                System.out.println("s = " + s);
-            }
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e){
