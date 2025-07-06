f06835a1b3282fac868b82786113d08211cc777f
jieun <yudaeun222@gmail.com>
2025-07-06 21:17:39 +0900

    엔티티 직접 사용

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -43,18 +43,12 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t from Team t ";
-            List<Team> result = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
-                    .getResultList();
+            String query = "select m from Member m where m = :member";
+            Member findMember = em.createQuery(query, Member.class)
+                    .setParameter("member",member1)
+                    .getSingleResult();
 
-            for(Team team : result){
-                System.out.println("team = " + team.getName() + "|members = " + team.getMembers().size());
-                for(Member member : team.getMembers()) {
-                    System.out.println(" -> member = " + member);
-                }
-            }
+            System.out.println("findMember = " + findMember);
 
             tx.commit();
         } catch (Exception e){
